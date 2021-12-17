c0fd3cd67b314443882c7f53f5bf06d634407ea6
voducminh1402 <voducminh140201@gmail.com>
2021-12-17 21:26:02 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,19 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test deploy
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
